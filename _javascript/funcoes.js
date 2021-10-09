9c419f21d6fc8616e602d290447b241246e7faa1
Shaffer443 <rafaelgouvieamelo@yahoo.com.br>
2021-10-09 11:00:01 -0300

    primeiro upload

---
+++ _javascript/funcoes.js
@@ -0,0 +1,8 @@
+
+alert("Site modelo pra estudos em HTML/CSS/JAVASCRIPT.\nNão tem função comercial. Apenas portfólio.");
+
+function mudafoto(foto){
+
+    document.getElementById("icone").src = foto;
+
+}
